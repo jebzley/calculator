84af339023d958cf64e56d7c5b06eb5b91a6e074
Seb Lee <sebastian.ma.lee@gmail.com>
2021-02-13 23:10:48 +0000

    minor end of day cleanup

--- scripts/maths.js
+++ scripts/maths.js
@@ -1,15 +1,15 @@
-// TODO
+// ** TODO **
+// frontend niceness: fix screen overflow for large numbers 
+// frontend niceness: make fullscreen on tablet/mobile
+// TIDY UP. reformat as switch/case. could help you solve below issue.
 // make (x)y work
-// fix screen overflow for large numbers
-// make fullscreen on tablet/mobile
 // make = perform the same calculation again on the result
-
-// put it all in switch/case rather than clunky if?
-
-// BONUS CHALLENGES
 // add scientific operators
 
-// DOM elements
+//---------------------------------------------------------------------------
+
+// ** VARIABLE DEFINITONS **
+
 const buttonElements = document.querySelectorAll(".calc__button");
 const outputDisplay = document.getElementById("output-display");
 const outputCalcArea = document.getElementById("output-display-calculation");
@@ -17,7 +17,6 @@ const outputEqualsArea = document.getElementById("output-display-equals");
 const outputTotalArea = document.getElementById("output-display-total");
 const previousCalcArea = document.getElementById("output-prev-total");
 
-// maths variables
 let currentCalculation = "";
 let currentNumber = "";
 let currentTotal = "";
@@ -26,96 +25,7 @@ let previousTotal = "";
 let displayCalculation = "";
 let bracketClosed = true;
 
-buttonElements.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    const clickedButtonValue = event.target.value;
-
-    // if there is already a total value stored, save it in case user wants to use it in next calculation
-    if (currentTotal != "") {
-      previousCalculation = currentCalculation;
-      previousTotal = currentTotal;
-      currentTotal = "";
-      previousCalcArea.innerHTML = `${formatDisplayCalculation(
-        displayCalculation
-      )} = ${previousTotal}`;
-      clearDisplay();
-      bracketClosed = true;
-
-      // if there is already a total value AND the user hits an operator, put previous total at the start of current calc
-      if (event.target.className == "calc__button calc__operator") {
-        currentNumber = previousTotal;
-        currentTotal = "";
-        clearDisplay();
-        displayCalculation = previousTotal;
-        outputCalcArea.innerHTML = displayCalculation;
-      } else {
-        currentNumber = "";
-        currentTotal = "";
-        clearDisplay();
-      }
-
-      // clear current total and wipe HTML elements
-    }
-
-    //if user clicks AC button, clear all data
-    if (clickedButtonValue == "delete") {
-      currentNumber = "";
-      currentCalculation = "";
-      currentTotal = "";
-      clearDisplay();
-    }
-
-    //if user hits equals button
-    else if (clickedButtonValue == "=") {
-      //  add the NUMBER to the CALCULATION
-      currentCalculation += currentNumber;
-
-      //  run the expression of CALCULATION and store it in TOTAL
-      console.log("to be calculated = " + currentCalculation);
-      let calculationAfterBrackets = calculateBrackets(currentCalculation);
-      console.log("after brackets = " + calculationAfterBrackets);
-      currentTotal = calculate(
-        parseStringAsCalculationArray(calculationAfterBrackets)
-      );
-
-      outputTotalArea.innerHTML = Number(currentTotal).toLocaleString();
-      outputEqualsArea.innerHTML = "=";
-      // and clear the current calculation
-      currentCalculation = "";
-      bracketClosed = true;
-    }
-
-    // if user presses brackets
-    else if (clickedButtonValue == "brackets") {
-      processBrackets();
-      console.log("number that's about to be processed =" + currentNumber);
-      currentNumber = allowBracketAsMultiplier(currentNumber);
-      outputCalcArea.innerHTML = formatDisplayCalculation(displayCalculation);
-    }
-
-    //if it's percent
-    else if (clickedButtonValue == "%") {
-      //print it on screen
-      displayCalculation += clickedButtonValue;
-      outputCalcArea.innerHTML = formatDisplayCalculation(displayCalculation);
-      currentNumber = calculatePercentage(currentCalculation, currentNumber);
-    }
-
-    //if it's an operator
-    else if (event.target.className == "calc__button calc__operator") {
-      currentCalculation += currentNumber;
-      currentCalculation += clickedButtonValue;
-      currentNumber = "";
-      displayCalculation += clickedButtonValue;
-      outputCalcArea.innerHTML = formatDisplayCalculation(displayCalculation);
-    } else {
-      //if it's a number, add it to NUMBER string
-      currentNumber += clickedButtonValue;
-      displayCalculation += clickedButtonValue;
-      outputCalcArea.innerHTML = formatDisplayCalculation(displayCalculation);
-    }
-  });
-});
+// ** FUNCTION DECLARATIONS **
 
 const formatDisplayCalculation = (stringForScreen) => {
   return stringForScreen
@@ -202,7 +112,6 @@ const calculatePercentage = (currentCalculation, currentNumber) => {
 };
 
 const processBrackets = () => {
-  // if previous was a bracket, add a * symbol
   if (bracketClosed) {
     currentNumber += "(";
     displayCalculation += "(";
@@ -228,19 +137,13 @@ const allowBracketAsMultiplier = (currentCalculation) => {
 const calculateBrackets = (currentCalculation) => {
   if (currentCalculation.includes(")")) {
     if (currentCalculation[1] == "(") {
-      console.log("opening bracket found at index 0");
       let splitCalc = currentCalculation.split(/[()]/g);
-      console.log("splitcalc = " + splitCalc);
       //splitCalc[0] = splitCalc[0].substring(1);
-      console.log("splitcalc[0] = " + splitCalc[1]);
       splitCalc[0] = calculate(parseStringAsCalculationArray(splitCalc[1]));
       return splitCalc.join("");
     } else {
-      console.log("opening bracket found");
       let splitCalc = currentCalculation.split(/[()]/g);
-      console.log("calculation split into: " + splitCalc);
       //splitCalc[1] = splitCalc[1].substring(0, splitCalc[1].length - 1);
-      console.log(splitCalc);
       splitCalc[1] = calculate(parseStringAsCalculationArray(splitCalc[1]));
       return splitCalc.join("");
     }
@@ -253,3 +156,94 @@ const clearDisplay = () => {
   outputEqualsArea.innerHTML = null;
   outputTotalArea.innerHTML = null;
 };
+
+// -------------------------------------------------------------------------
+// ** CALCULATOR STARTS HERE **
+buttonElements.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const clickedButtonValue = event.target.value;
+
+    // if there is already a total value stored, save it 
+    if (currentTotal != "") {
+      previousCalculation = currentCalculation;
+      previousTotal = currentTotal;
+      currentTotal = "";
+      previousCalcArea.innerHTML = `${formatDisplayCalculation(
+        displayCalculation
+      )} = ${previousTotal}`;
+      clearDisplay();
+      bracketClosed = true;
+
+      // if there is already a total value AND the user hits an operator
+      if (event.target.className == "calc__button calc__operator") {
+        currentNumber = previousTotal;
+        currentTotal = "";
+        clearDisplay();
+        displayCalculation = previousTotal;
+        outputCalcArea.innerHTML = displayCalculation;
+      } else {
+        currentNumber = "";
+        currentTotal = "";
+        clearDisplay();
+      }
+    }
+
+    //if user clicks AC button, clear all data
+    if (clickedButtonValue == "delete") {
+      currentNumber = "";
+      currentCalculation = "";
+      currentTotal = "";
+      clearDisplay();
+    }
+
+    //if user hits equals button
+    else if (clickedButtonValue == "=") {
+      //  add the NUMBER to the CALCULATION
+      currentCalculation += currentNumber;
+
+      //  run the expression of CALCULATION and store it in TOTAL
+      console.log("to be calculated = " + currentCalculation);
+      let calculationAfterBrackets = calculateBrackets(currentCalculation);
+      console.log("after brackets = " + calculationAfterBrackets);
+      currentTotal = calculate(
+        parseStringAsCalculationArray(calculationAfterBrackets)
+      );
+
+      outputTotalArea.innerHTML = Number(currentTotal).toLocaleString();
+      outputEqualsArea.innerHTML = "=";
+      // and clear the current calculation
+      currentCalculation = "";
+      bracketClosed = true;
+    }
+
+    // if user presses brackets
+    else if (clickedButtonValue == "brackets") {
+      processBrackets();
+      console.log("number that's about to be processed =" + currentNumber);
+      currentNumber = allowBracketAsMultiplier(currentNumber);
+      outputCalcArea.innerHTML = formatDisplayCalculation(displayCalculation);
+    }
+
+    //if it's percent
+    else if (clickedButtonValue == "%") {
+      //print it on screen
+      displayCalculation += clickedButtonValue;
+      outputCalcArea.innerHTML = formatDisplayCalculation(displayCalculation);
+      currentNumber = calculatePercentage(currentCalculation, currentNumber);
+    }
+
+    //if it's an operator
+    else if (event.target.className == "calc__button calc__operator") {
+      currentCalculation += currentNumber;
+      currentCalculation += clickedButtonValue;
+      currentNumber = "";
+      displayCalculation += clickedButtonValue;
+      outputCalcArea.innerHTML = formatDisplayCalculation(displayCalculation);
+    } else {
+      //if it's a number, add it to NUMBER string
+      currentNumber += clickedButtonValue;
+      displayCalculation += clickedButtonValue;
+      outputCalcArea.innerHTML = formatDisplayCalculation(displayCalculation);
+    }
+  });
+});
